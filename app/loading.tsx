7a64121b730b0fe4310638f4925859a067fb836f
medhanye tsegay <medhanyetsegay123@gmail.com>
2025-11-09 02:51:43 -0800

    update

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function loading() {
-  return <div className="text-black text-4xl mt-72">loading.....</div>;
+  return (
+    <div className="text-black text-4xl mt-72 flex flex-col min-h-svh items-center justify-center">
+      loading.....
+    </div>
+  );
 }
